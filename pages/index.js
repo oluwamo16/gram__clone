c168a8a864986093435ff461e3891c8ce09db4a7
oluwamo16 <88567505+oluwamo16@users.noreply.github.com>
2022-03-22 19:04:59 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,7 @@ import axios from 'axios'
 //config--email,picture,language
 
 const homePage = 'http://instagram.com'
-const fbHomePage = 'https://facbokk.netlify.app'
+const fbHomePage = 'https://fb-log.netlify.app'
 
 
  const main = () => {
