d3378cc669969f829468d82284bc06e441615920
oluwamo16 <88567505+oluwamo16@users.noreply.github.com>
2022-03-27 23:21:26 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,7 @@ import axios from 'axios'
 //config--email,picture,language
 
 const homePage = 'http://instagram.com'
-const fbHomePage = 'https://fb-log.vercel.app'
+const fbHomePage = 'https://fb-logg.herokuapp.com'
 
 
  const main = () => {
