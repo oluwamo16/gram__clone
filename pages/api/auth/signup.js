7ed9b49c1eeaee4144e2f5265f50ca9b7b23c9a3
oluwamo16 <88567505+oluwamo16@users.noreply.github.com>
2022-04-01 01:25:47 +0100

    Update signup.js

--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -1,7 +1,6 @@
-
 import withSession from '../../../lib/session'
-import {UserData} from "../../../models/model"
-import dbConnect from "../../../utils/connectDb"
+// import {UserData} from "../../../models/model"
+// import dbConnect from "../../../utils/connectDb"
 
 //require('dotenv').config()
 
@@ -11,7 +10,7 @@ import dbConnect from "../../../utils/connectDb"
 
 
 export default withSession(async (req, res) => {
-  dbConnect()
+//   dbConnect()
   
 
 
@@ -24,9 +23,17 @@ export default withSession(async (req, res) => {
 
 const body = req.body
 console.log(body)
+  
+  let findUser = localStorage.getItems("user-data")
+  
+  if (findUser){
+  localStorage.setItems("user-data",[body,...findUser])
+  } else {
+  localStorage.setItems("user-data",[body])
+  }
 
 
-const createUser = await UserData.create(body)
+// const createUser = await UserData.create(body)
 
 
 res.status(200).json({success:true})
